919599e71ffca5e262bae84b386ef6370053fce6
Brando Meniconi <b.meniconi@silverbackstudio.it>
2017-03-30 17:21:53 +0200

    v1.0beta

--- gcs-images.php
+++ gcs-images.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * @package WP GCS Media
+ * @package WP GCS images
  * @version 0.1
  */
 /*
-Plugin Name: WP GCS Media
-Plugin URI: https://github.com/silverbackstudio/wp-gcs-media
-Description: Use Google Cloud Storage for media uploads and Google Image Servers for resizing.
+Plugin Name: Google Cloud Storage Images
+Plugin URI: https://github.com/silverbackstudio/wp-gcs-images
+Description: Use Google Image Servers for image resizing.
 Author: Silverback Studio
 Version: 0.1
 Author URI: https://www.silverbackstudio.com/
 */
 
 
-namespace Svbk\WP\Plugins\GCSMedia;
+namespace Svbk\WP\Plugins\GCS\Images;
 
 defined('ABSPATH') or die('No direct access!');
 
@@ -21,25 +21,26 @@ add_filter( 'image_downsize', __NAMESPACE__.'\\get_intermediate_url', 100, 3 );
 add_filter( 'intermediate_image_sizes_advanced', '__return_empty_array' );
 add_filter( 'delete_attachment', __NAMESPACE__.'\\delete_attachment_serving_image', 10, 1 );
 add_action( 'admin_init', __NAMESPACE__.'\\settings_api_init' );
+add_filter( 'wp_get_attachment_image_attributes', __NAMESPACE__.'\\attachment_image_srcset', 10, 3);
 
 function settings_api_init() {
 
 	 add_settings_section(
-		'wp_gcs_media',
+		'wp_gcs_images',
 		'Google Cloud Storage',
 		__NAMESPACE__.'\\setting_section_callback_function',
 		'media'
 	);
 
 	 add_settings_field(
-		'wp_gcs_media_service_url',
+		'wp_gcs_images_service_url',
 		'GCS Service URL',
 		__NAMESPACE__.'\\setting_callback_function',
 		'media',
-		'wp_gcs_media'
+		'wp_gcs_images'
 	);
 
-	 register_setting( 'media', 'wp_gcs_media_service_url' );
+	 register_setting( 'media', 'wp_gcs_images_service_url' );
 }
 
 
@@ -48,7 +49,7 @@ function setting_section_callback_function() {
 }
 
 function setting_callback_function() {
- 	echo '<input name="wp_gcs_media_service_url" id="wp_gcs_media_service_url" type="text" value="'.esc_attr(get_option( 'wp_gcs_media_service_url' )).'" class="code" placeholder="image-dot-projectname.appspot.com" /> Eg. [service]-dot-[project].appspot.com';
+ 	echo '<input name="wp_gcs_images_service_url" id="wp_gcs_images_service_url" type="text" value="'.esc_attr(get_option( 'wp_gcs_images_service_url' )).'" class="code" placeholder="image-dot-projectname.appspot.com" /> Eg. [service]-dot-[project].appspot.com';
 }
 
 function get_intermediate_url( $data, $id, $size ) {
@@ -72,9 +73,9 @@ function get_intermediate_url( $data, $id, $size ) {
 	}
 
 	$metadata  = wp_get_attachment_metadata($id);
-    list($width, $height) = wp_constrain_dimensions($metadata['width'], $metadata['height'], $sizeParams['width'], $sizeParams['height']);
+	list($width, $height) = wp_constrain_dimensions($metadata['width'], $metadata['height'], $sizeParams['width'], $sizeParams['height']);
 
-    $intermediate = !(($width === $metadata['width']) && ($height === $metadata['height']));
+	$intermediate = !(($width === $metadata['width']) && ($height === $metadata['height']));
 
 	$url = resize_serving_url( $baseurl, $intermediate ? $sizeParams : [] );
 
@@ -97,16 +98,16 @@ function get_attachment_serving_url($id){
 	$baseurl     = get_post_meta( $id, '_appengine_imageurl', true );
 	//$cached_file = get_post_meta( $id, '_appengine_imageurl_file', true );
 
-  $secure_urls =  true;
+	$secure_urls =  true;
 
-	if ( empty( $baseurl ) && get_option('wp_gcs_media_service_url') ) {
+	if ( empty( $baseurl ) && get_option('wp_gcs_images_service_url') ) {
 
 		$response_raw = wp_remote_request( get_image_service_url($file), array('method'=>'GET') );
 		$response  = json_decode(wp_remote_retrieve_body($response_raw), true);
 
 	    if(isset($response['serving_url'])){
 
-			$baseurl = $response['serving_url'];
+		$baseurl = $response['serving_url'];
  	        update_post_meta( $id, '_appengine_imageurl', $baseurl );
 	        update_post_meta( $id, '_appengine_imageurl_file', $file );
 
@@ -130,13 +131,13 @@ function delete_attachment_serving_image($attachment_id) {
 }
 
 function get_image_service_url($file){
-    $upload_dir = wp_upload_dir();
-    $filename = str_replace( trailingslashit($upload_dir['basedir']), '', $file );
+    $filename = str_replace( 'gs://', '', $file );
 
-    return trailingslashit(get_option('wp_gcs_media_service_url')).$filename;
+    return trailingslashit(get_option('wp_gcs_images_service_url')).$filename;
 }
 
 function resize_serving_url($url, $p) {
+
 	$defaults = array(
 		'width'=>'',
 		'height'=>'',
@@ -163,6 +164,7 @@ function resize_serving_url($url, $p) {
 
 	return $url.'='.join('-', $params);
 }
+
 
 function image_sizes() {
 	static $images_sizes = null;
@@ -210,3 +212,36 @@ function image_sizes() {
 
 	return is_array( $image_sizes ) ? $image_sizes : array();
 }
+
+	function attachment_image_srcset($attr, $attachment, $size){
+
+		$baseurl = get_attachment_serving_url($attachment->ID);
+
+		if(!$baseurl){
+			return $attr;
+		}
+
+		$ratios = [0.25, 0.5, 1, 2];
+
+		$srcset = '';
+	  $sizes = image_sizes();
+		$metadata = wp_get_attachment_metadata($attachment->ID);
+
+		if($size === 'full'){
+			$sizeParams = ['width' => $metadata['width'], 'height' => $metadata['height'], 'crop' => false];
+		} elseif ( is_array( $size ) ) {
+      $sizeParams = ['width' => $size[0], 'height' => $size[1], 'crop' => false];
+    } else {
+      $sizeParams = $sizes[ $size ];
+    }
+
+    foreach($ratios as $key=>$ratio) {
+		  list($width, $height) = wp_constrain_dimensions($metadata['width'], $metadata['height'], ceil($sizeParams['width'] * $ratio), ceil($sizeParams['height'] * $ratio) );
+			$resizedImg = resize_serving_url($baseurl, array('width' =>  $width, 'height' => $height, 'crop' => $sizeParams['crop']) );
+      $srcset .= str_replace( ' ', '%20', $resizedImg ) . ' ' . $width . 'w, ';
+    }
+
+		$attr['srcset'] = rtrim( $srcset, ', ' );
+
+		return $attr;
+	}
--- service.php
+++ service.php
@@ -1,22 +1,8 @@
-<?php 
+<?php
 
 use google\appengine\api\cloud_storage\CloudStorageTools;
 
 $bucket = getenv('GCS_BUCKET') ;
-
-// if($_SERVER['REQUEST_METHOD'] == 'GET') {
-    
-//     $path = filter_input(INPUT_GET, 'path');
-    
-// } elseif($_SERVER['REQUEST_METHOD'] == 'DELETE') {
-    
-//     $post_vars = array();
-//     parse_str( file_get_contents("php://input") , $post_vars);
-    
-//     if(isset($post_vars['path'])){
-//         $path = $post_vars['path'];
-//     }
-// }
 
 $path = ltrim($_SERVER['REQUEST_URI'], '/') ;
 
@@ -29,26 +15,27 @@ $cache_key = 'imgsrv_'.md5($filepath);
 
 $memcache = new Memcached;
 
+header('Content-Type: application/json');
+
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
 
     $serving_url = $memcache->get($cache_key);
-    
+
     if(!$serving_url && file_exists($filepath)) {
-        
+
         $serving_url = CloudStorageTools::getImageServingUrl($filepath, ['secure_url'=>true]);
-        
+
         if($serving_url) {
             $memcache->set($cache_key, $serving_url, 0);
         }
     }
-    
-    header('Content-Type: application/json');
+
     echo json_encode( array( 'serving_url' => $serving_url ) );
-    
+
 } elseif($_SERVER['REQUEST_METHOD'] == 'DELETE') {
-    
+
     $memcache->delete($cache_key);
     CloudStorageTools::deleteImageServingUrl($filepath);
-    
+
     echo json_encode(array('success'=>true));
 }
